4e1e12a83e6973bff4ba910829f361ed3dec030a
Ivaan2 <ivan.m.m.2002.ivi@gmail.com>
2022-08-26 08:22:41 +0200

    home incluido

--- flo_joe/flo_joe/settings.py
+++ flo_joe/flo_joe/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'phrasal_verbs',
     'jquery',
+    'home'
 ]
 
 MIDDLEWARE = [
--- flo_joe/flo_joe/urls.py
+++ flo_joe/flo_joe/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('phrasal_verbs/', include('phrasal_verbs.urls')),
+    path('', include('home.urls'))
 ]
